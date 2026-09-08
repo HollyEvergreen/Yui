df8531e49c8b7c7aa771177fba9fe96ec23b3ad5
Holly <itshollyevergreen@gmail.com>
2026-09-08 10:38:20 +0100

    added window ogl ctx input ctx and imgui controller along with window config auto saving and Config getting within the filesystem api

--- App.cs
+++ App.cs
@@ -1,30 +1,54 @@
+using ImGuiNET;
+using Silk.NET.Input;
+using Silk.NET.OpenGL;
+using Silk.NET.OpenGL.Extensions.ImGui;
+using Silk.NET.Windowing;
+
 namespace Yui;
 
 public abstract class App
 {
-	protected Filesystem? fs;
+	protected Filesystem fs = new();
 	private double _dt;
-	public int run()
-	{
-		on_load();
+	private Timer windowAutoSave;
+	private long ASPeriod = 300_000;
+    protected TimeSpan AutoSavePeriod
+    {
+        get => new(ASPeriod * 10_000);
+        set => ASPeriod = value.Ticks / 10_000;
+    }
+    public IWindow? Window { get; private set; }
+    public GL? Gl { get; private set; }
+	public IInputContext? Input {get; private set;}
+    public ImGuiController? Imgui { get; private set; }
 
-		Start();
-		while (true)
-		{
-			var _s = DateTime.Now;
-			update(_dt);
-			render(_dt);
-			_dt = (DateTime.Now - _s).Ticks;
-		}
+    private void WinAutoSave(object? window)
+    {
+        
+    }
+
+    public int run()
+	{
+        Window = Silk.NET.Windowing.Window.Create(fs.GetConfig<WindowOptions>("Window"));
+		Gl = Window.CreateOpenGL();
+		Input = Window.CreateInput();
+        Imgui = new ImGuiController(Gl, Window, Input);
+		windowAutoSave = new(WinAutoSave, null, ASPeriod, ASPeriod);
+		Window.Load += on_load;
+		Window.Load += Start;
+		Window.Update += update;
+		Window.Render += render;
+
+		Window.Run();
 		return 0;
 	}
 	private void on_load()
 	{
-		fs = new Filesystem();
 		OnLoad();
 	}
 	private void update(double dt)
 	{
+
 		Update(dt);
 	}
 	private void render(double dt)
--- Filesystem.cs
+++ Filesystem.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using Tomlyn;
 
 namespace Yui;
 
@@ -12,10 +13,44 @@ public class Filesystem
 		{
 			BaseDir.Create();
 		}
+		FileObjects?.Add("Window", new FileInfo(@""));
 	}
 	private Filesystem(string path)
 	{
 		BaseDir = new DirectoryInfo(Path.GetFullPath(path));
 	}
 	public DirectoryInfo BaseDir { get; private set; }
+	private readonly Dictionary<string, FileObject> FileObjects = [];
+
+    internal T? GetConfig<T>(string FileObject) => 
+		TomlSerializer.
+			Deserialize<T>(
+				File.ReadAllText(
+					GetPath(FileObject)
+				)
+			);
+	private string GetPath(string fileObject)
+	{
+		var Path = FileObjects[fileObject].Obj.FullName;
+		return BaseDir + Path;
+	}
+}
+record FileObject(bool IsFile, FileSystemInfo Obj)
+{
+	public static implicit operator FileObject(FileInfo finfo)
+	{
+		return new(true, finfo);
+	}
+	public static implicit operator FileObject(DirectoryInfo dinfo)
+	{
+		return new(false, dinfo);
+	}
+	public static implicit operator FileInfo?(FileObject fobj)
+	{
+		return fobj.Obj as FileInfo;
+	}
+	public static implicit operator DirectoryInfo?(FileObject fobj)
+	{
+		return fobj.Obj as DirectoryInfo;
+	}
 }
