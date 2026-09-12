b0a4907050df02b8199a752c5ca1ede568f0e861
Holly <itshollyevergreen@gmail.com>
2026-09-12 15:12:43 +0100

    improved filesystem access

--- Filesystem.cs
+++ Filesystem.cs
@@ -43,21 +43,54 @@ public class Filesystem
 		}
 	}
 	public DirectoryInfo BaseDir { get; private set; } = null!;
-	private readonly Dictionary<string, FileSystemInfo> FileObjects = [];
+	private readonly Dictionary<string, FileInfo> FileObjects = [];
+	private readonly Dictionary<string, FileInfo> DirectoryObjects = [];
 
 	public Target? GetConfig<Target>(string FileObject, TomlSerializerOptions? opt = null) =>
 		TomlSerializer.Deserialize<Target>(
-			File.ReadAllText(GetPath(FileObject)), 
+			File.ReadAllText(GetFileObjectPath(FileObject)), 
 			opt
 		);
 	public void WriteConfig<T>(string name, T val, TomlSerializerOptions? opt = null)
     {
 	    TomlSerializer.Serialize(
-		    GetFile(name),
+		    GetFileObject(name),
 		    val,
 		    opt
 	    );
     }
-    private string GetPath(string fileObject) => FileObjects[fileObject].FullName;
-    private FileStream GetFile(string fileObject) => (FileObjects[fileObject] as FileInfo)?.Open(FileMode.OpenOrCreate)!;
+	private string GetFileObjectPath(string fileObject) => FileObjects[fileObject].FullName;
+	private string GetPath(string path) => BaseDir + path;
+    private FileStream GetFileObject(string fileObject) => (FileObjects[fileObject] as FileInfo)?.Open(FileMode.OpenOrCreate)!;
+    public BinaryWriter OpenBin(string path) => new(GetFile(GetPath(path)));
+    private static FileStream GetFile(string Path) => File.Open(Path, FileMode.OpenOrCreate);
+    public void DeclareConfig(string name, string path)
+    {
+	    var file = new FileInfo(BaseDir +"/"+ path);
+	    FileObjects.Add(name, file);
+	    if (file.Directory is { Exists: true }) file.Directory.Create();
+	    if (!file.Exists)
+	    {
+		    var f = file.CreateText();
+		    f.Dispose();
+		    f.Close();
+	    }
+    }
+    public void Foreach(string Path, string pattern, Action<FileInfo> action) => new DirectoryInfo(BaseDir + Path).EnumerateFiles(pattern).ToList().ForEach(action);
+    public void PrintConfig<T>(T val, TomlSerializerOptions opts) =>
+	    TomlSerializer.Serialize(
+		    Console.OpenStandardOutput(),
+		    val,
+		    opts
+	    );
+    public void WriteConfigAt<T>(string path, T config, TomlSerializerOptions opts)
+    {
+	    using var file = File.OpenWrite(GetPath(path));
+	    TomlSerializer.Serialize(
+		    file,
+		    config,
+		    opts
+	    );
+	    file.Flush(true);
+    }
 }
